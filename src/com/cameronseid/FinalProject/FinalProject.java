47c9b24956e88f12fc1c035ba8b3ed1f17972074
techniponi <wincam97@gmail.com>
2017-05-04 05:50:33 -0500

    Initial commit

---
+++ src/com/cameronseid/FinalProject/FinalProject.java
@@ -0,0 +1,13 @@
+package com.cameronseid.FinalProject;
+
+public class FinalProject {
+
+    public static void main(String[] args) {
+
+        boolean gameRunning = false; // states whether a game is currently in progress
+
+        System.out.println("\n\n*** GARDEN ***\n\nFinal project for Intro to Programming Fundamentals\nDeveloped by Cameron Seid (me@cameronseid.com)\n\nAvailable on GitHub! https://github.com/techniponi/finalproject\nPublished under the MIT License, see LICENSE file for details.\n\n");
+
+    }
+
+}
